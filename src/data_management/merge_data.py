1c375ae4c5c3d1c95d0bbc28ceb6d06ccfdacf00
Elif Bodur <elifbodur@uni-bonn.de>
2019-02-27 16:50:44 +0100

    Merged dataset 'data_all.dta' is changed to 'data_all.csv'.

--- src/data_management/merge_data.py
+++ src/data_management/merge_data.py
@@ -4,10 +4,7 @@
 import numpy as np
 import pandas as pd
 import pickle
-# =============================================================================
-# import os
-# os.chdir('C:\\Users\\ASUS\\Desktop\\isler\\Bonn\\Winter_2018\\EPP\\epp_project\\epp_stata\\epp_final_project\\bld\\out\\data')
-# =============================================================================
+
 from bld.project_paths import project_paths_join as ppj
 
 
@@ -31,18 +28,9 @@ for f in range(len(f_nr)):
         left_on='factor_id', right_on=f_nr[f]
     )
     
-data = df[0].append([df[1], df[2]])
-data_all = data.to_csv(ppj("OUT_DATA", "data_all.dta"))
-# =============================================================================
-# data_all = data.to_csv("data_all.csv"), sep=",")
-# def save_data(sample):
-#     sample.tofile(ppj("OUT_DATA", "data_all.csv"), sep=",")
-# 
-# 
-# if __name__ == "__main__":
-#     sample = data_all
-#     save_data(sample)
-# =============================================================================
+data = df[0].append([df[1], df[2]]).set_index(['case_id', 't'])
+data_all = data.to_csv(ppj("OUT_DATA", "data_all.csv"), sep=",")
+
 
 
 
