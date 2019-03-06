2364aef4f5ff8b801ec54f1d43cd737348ba17d6
Maximilian Weiß <maximilian.weiss@gmail.com>
2019-03-06 16:16:09 +0100

    Added marginal_probability-method in Transition class and wrote test for it.

--- src/analysis/test_transition.py
+++ src/analysis/test_transition.py
@@ -35,6 +35,36 @@ def expected_1nonconst_factor_next_state():
                            ])
     return out
 
+@pytest.fixture
+def setup_1nonconst_factor_marg_prob():
+    out = {}
+    params = {
+                'phi': 1, 'lambda': 1, 'gamma1': 1, 'gamma2': 0, 'gamma3': 0,
+                'var_u': 0.01
+             }
+    out['trans_obj'] = Transition([params], [1, 0, 0])
+    out['state'] = np.array(
+                            [[[1, 1], [1, 1]],
+                             [[2, 3], [4, 5]],
+                             [[6, 7], [8, 9]]]
+                            )
+    out['next_state'] = np.array(
+                                    [[1.1, 0.9],
+                                     [3, 4],
+                                     [7, 8]]
+                                )
+    return out
+
+@pytest.fixture
+def expected_1nonconst_factor_marg_prob():
+    out = {}
+    prob = np.exp(-.5)/np.sqrt(0.02*np.pi)
+    out['marg_prob'] = np.array(
+                                    [[0, prob],
+                                     [prob, 0]]
+                                )
+    return out
+
 def test_next_state_1nonconst_factor(
                                         setup_1nonconst_factor_next_state,
                                         expected_1nonconst_factor_next_state
@@ -48,3 +78,16 @@ def test_next_state_1nonconst_factor(
                         next_state,
                         expected_1nonconst_factor_next_state['next']
                        )
+def test_marginal_probability(
+                                setup_1nonconst_factor_marg_prob,
+                                expected_1nonconst_factor_marg_prob
+                             ):
+    trans = setup_1nonconst_factor_marg_prob['trans_obj']
+    marginal = trans.marginal_probability(
+                                setup_1nonconst_factor_marg_prob['next_state'],
+                                setup_1nonconst_factor_marg_prob['state']
+                                         )
+    assert_array_equal(
+                        marginal,
+                        expected_1nonconst_factor_marg_prob['marg_prob']
+                      )
