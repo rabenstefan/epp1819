14a563220440a72220a8747cd0fa23915e85eb83
Maximilian Weiß <maximilian.weiss@gmail.com>
2019-03-06 21:54:15 +0100

    Changed name of csv-file.

--- src/final/plot_differences.py
+++ src/final/plot_differences.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from bld.project_paths import project_paths_join as ppj
 
 def plot_differences():
-    estimates = pd.read_csv(ppj('OUT_ANALYSIS','estimated_factors.csv'))
+    estimates = pd.read_csv(ppj('OUT_ANALYSIS','factor_estimates.csv'))
     truth = pd.read_pickle(ppj('OUT_ANALYSIS','true_facs.pkl'))
     diff = estimates - truth
     bp_fac3 = sns.boxplot(x = diff.loc[(slice(None),1),'fac3'])
